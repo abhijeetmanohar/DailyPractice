b3e77df38bfec7ee9d8a5d47f1b726fe61917b90
Abhijeet Manohar <abhijeetheromanohar@gmail.com>
2020-10-06 23:44:39 -0500

    Create LinkedListIntersection_Oct6.java

---
+++ LinkedListIntersection_Oct6.java
@@ -0,0 +1,33 @@
+//Intersection of Two Linked Lists - Leet Code - Easy
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        
+        while(tempA != null){
+            //Compare each node of A to each of B.
+            tempB = headB;
+            while(tempB != null){
+                if(tempA == tempB){
+                    return tempA;
+                }
+                tempB = tempB.next;
+            }
+            tempA = tempA.next;
+        }
+        return null;
+    }
+    
+}
